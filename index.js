75e3a0ea2ec583c96cc8e337f3980b712bdf7cdb
unknown <0401luna@naver.com>
2024-08-24 13:00:09 +0900

    First C o m m i t

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://luna:123$@luna.t5kxd.mongodb.net/?retryWrites=true&w=majority&appName=luna",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("M o n g o D B C o n e c t"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(port, () =>
+  console.log(`port: ${port} l i s t e n`)
+);
---
+++ model/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: string,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, // 공백 제거
+    unique: 1,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: number,
+  },
+});
